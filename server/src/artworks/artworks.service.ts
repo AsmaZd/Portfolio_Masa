074172793b14f61a059f78f33b5f63b398d8588b
AsmaZd <asmazaied@outlook.fr>
2026-09-05 15:05:48 +0200

    refactor(backend): add a '/schemas' and '/dto' folder to artworks

--- server/src/artworks/artworks.service.ts
+++ server/src/artworks/artworks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { Artwork } from "./artwork.schema";
+import { Artwork } from "./schemas/artwork.schema";
 
 @Injectable()
 export class ArtworksService {
